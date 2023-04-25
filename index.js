79b53e2aaab2cbfdd3af6b26fff9ceccbdefb508
shanichikondi <131780404+shanichikondi@users.noreply.github.com>
2023-04-25 14:33:53 +0100

    add passenger counter files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//document.getElementById("count-el").innerText = 5
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countstr = count + " - "
+    saveEl.textContent += countstr
+    countEl.textContent = 0
+    count = 0
+}
+
